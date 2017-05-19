b87d860235e5bd39ef965a206389a99f5f21ab1b
Marek Ištvánek <marek.istvanek@ce-ma-s.net>
2017-05-19 12:51:09 +0200

    Application info

--- Pir/Properties/AssemblyInfo.cs
+++ Pir/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Pir")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PIR")]
+[assembly: AssemblyDescription("Plasmatic Implosion Reactor")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pir")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("CeMaS")]
+[assembly: AssemblyProduct("PIR")]
+[assembly: AssemblyCopyright("Free Spirit - 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
